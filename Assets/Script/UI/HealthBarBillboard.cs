75473c30ba299184397b113783c68a5e38696b24
leodimano <leodimano@live.com>
2016-06-15 23:36:51 -0300

    Implementação da barra de Vida dos Inimigos

---
+++ Assets/Script/UI/HealthBarBillboard.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBarBillboard : MonoBehaviour
+{
+    Character _character;
+    Transform _healthBarPivot;
+    Transform _healthBar;
+
+    float _healthPercentual;
+    Vector3 _lastPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        _character = GetComponentInParent<Character>();
+        _healthBar = this.transform.FindChild("HealthBar");
+        _healthBarPivot = _healthBar.FindChild("HealthBar_Pivot");
+
+        if (_healthBar != null)
+            _healthBar.gameObject.SetActive(false);
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.LookAt(new Vector3(transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z));
+        // this.transform.eulerAngles = new Vector3(this.transform.rotation.x, 0, 0);
+
+        if (_character != null && _healthBarPivot != null)
+        {
+            _healthPercentual = _character.HitPoint.CurrentWithModifiers / _character.HitPoint.MaxWithModifiers;
+
+            if (_healthPercentual > 0 && _healthPercentual < 1)
+            {
+                _healthBar.gameObject.SetActive(true);
+                _healthBarPivot.localScale = new Vector3(_character.HitPoint.CurrentWithModifiers / _character.HitPoint.MaxWithModifiers, 1, 1);
+            }
+            else
+            {
+                _healthBar.gameObject.SetActive(false);
+            }
+        }
+    }
+}
