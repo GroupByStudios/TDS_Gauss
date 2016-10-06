362a9f9f230d4de18e61f8ac8b39ce77dc432311
DevDesigner <brunolopesdesigner@gmail.com>
2016-10-06 02:28:56 -0300

    correção

--- Assets/Script/Application/PlayerManager.cs
+++ Assets/Script/Application/PlayerManager.cs
@@ -165,7 +165,7 @@ public class PlayerManager : MonoBehaviour
 			myPlayerAvatarList [2].gameObject.active== false &&
 			myPlayerAvatarList [3].gameObject.active== false &&
 			myPlayerAvatarList [4].gameObject.active == false &&
-			Input.GetKey(KeyCode.Space) {
+			Input.GetKey("escape")) {
 			Application.Quit ();
 		}
 
